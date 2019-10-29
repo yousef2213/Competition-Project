436a6140479e2977a17a8c4f0ac9c9c53176423a
yousef2213 <mandooyousef988@gmail.com>
2019-10-29 08:34:33 +0200

    Change 2

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function show(){
+    document.getElementById("btnn").innerHTML = "Chick Me";
+}
+
+function hiden(){
+    document.getElementById("btnn").innerHTML = "Get Start";
+}
+function showw(){
+    document.getElementById("btnn-2").innerHTML = "Chick Me";
+}
+
+function hidenn(){
+    document.getElementById("btnn-2").innerHTML = "Get Start";
+}
+function showww(){
+    document.getElementById("btnn-3").innerHTML = "Chick Me";
+}
+
+function hidennn(){
+    document.getElementById("btnn-3").innerHTML = "Get Start";
+}
